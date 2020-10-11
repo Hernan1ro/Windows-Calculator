f776e192adf8924cffd4b8fca511bcc5d894d2d3
Hernán Mercado <hernandmf@gmail.com>
2020-10-11 03:29:34 -0500

    Funtion procesador was added to capture numbers values

--- Js/Codigo.js
+++ Js/Codigo.js
@@ -1,12 +1,40 @@
-'use strict';
+"use strict";
 
-var d = document, pantalla = d.getElementById("pantalla"), teclas = d.querySelectorAll(".tecla span");
+var d = document,
+  pantalla = d.getElementById("pantalla"),
+  teclas = d.querySelectorAll(".tecla span"),
+  UltOperación = "",
+  resultado = 0,
+  principio = true,
+  cal = true;
 
-console.log(pantalla)
-console.log(teclas[5].innerHTML);
+function procesador(entrada) {
+  console.log(entrada);
+  var val = pantalla.textContent;
+  if (entrada == "<") {
+  } else {
+    if (entrada == "C") {
+      pantalla.textContent = "0";
+      resultado = 0;
+      principio = true;
+      cal = true;
+    } else {
+      if (principio == true) {
+        val = "";
+        pantalla.textContent = val;
+        principio = false;
+      }
+      pantalla.textContent = entrada;
+    }
+  }
+}
 
-teclas.forEach(function(elemento){
-   console.log(elemento.textContent); 
-})
+teclas.forEach(function (elemento) {
+  elemento.addEventListener("click", function () {
+    var entrada = this.textContent;
 
-
+    if (entrada != "") {
+      procesador(entrada);
+    }
+  });
+});
