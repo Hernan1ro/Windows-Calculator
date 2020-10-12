05a89c06e04c0630d40deee0eb8f640ee6f12694
Hernán Mercado <hernandmf@gmail.com>
2020-10-12 14:42:10 -0500

    Aritmetics operations has been added to the windows calculator

--- Js/Codigo.js
+++ Js/Codigo.js
@@ -11,7 +11,16 @@ var d = document,
 function procesador(entrada) {
   console.log(entrada);
   var val = pantalla.textContent;
+  if (entrada == "." && val.indexOf(".") > 1) return;
   if (entrada == "<") {
+    if (val.length > 1) {
+      val = val.substring(0, val.length - 1);
+      pantalla.textContent = val;
+    } else {
+      pantalla.textContent = "0";
+      principio = 1;
+      cal = 1;
+    }
   } else {
     if (entrada == "C") {
       pantalla.textContent = "0";
@@ -23,9 +32,53 @@ function procesador(entrada) {
         val = "";
         pantalla.textContent = val;
         principio = false;
+      } else {
       }
-      pantalla.textContent = entrada;
+      if (
+        entrada == "+" ||
+        entrada == "-" ||
+        entrada == "/" ||
+        entrada == "X"
+      ) {
+        principio = true;
+        if (cal) {
+          resultado = val;
+          cal = false;
+        } else {
+          calculador(val);
+          pantalla.textContent = resultado;
+        }
+        UltOperación = entrada;
+      } else {
+        if (entrada == "=") {
+          calculador(val);
+          pantalla.textContent = resultado;
+          cal = true;
+          principio = true;
+        } else {
+          pantalla.textContent = val + entrada;
+        }
+      }
     }
+  }
+}
+
+function calculador(val) {
+  val = parseFloat(val);
+  resultado = parseFloat(resultado);
+  switch (UltOperación) {
+    case "+":
+      resultado += val;
+      break;
+    case "-":
+      resultado -= val;
+      break;
+    case "X":
+      resultado *= val;
+      break;
+    case "/":
+      resultado /= val;
+      break;
   }
 }
 
